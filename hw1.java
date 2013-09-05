bb5853084d6c4af900a32c4d70621b8d7470472a
Ncoxy <truelonghornfan@comcast.net>
2013-09-05 09:30:30 -0600

    set up exercise 3

---
+++ hw1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class hw1 {
+	public static void main(String[] args) {
+		three(3);
+	}
+	public void three(int x) {
+		Scanner scanner = Scanner(System.in);
+		
+	}
+}

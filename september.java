a0879ca0713efef8e15c650ad2986a8929c77da9
Ncoxy <truelonghornfan@comcast.net>
2013-09-05 08:58:59 -0600

    Completed the secnd problem and started on the last problem

--- september.java
+++ september.java
@@ -1,6 +1,9 @@
 public class september {
 	public static void main(String[] args) {
-		zero();
+		// zero();
+		// one(2, 5);
+		two();
+
 	}
 	public static void zero() {
 		int sum = 0;
@@ -16,10 +19,20 @@ public class september {
 
 					
 	}
-	public static void one() {
+	public static void one(int x, int n) {
+		int exp = x;
 
+		while (n > 1){
+			exp = exp  * x;
+			n--; 
+			
 	}
+	System.out.println(sum);
+}
 	public static void two() {
-
+		int fact = x;
+		for (int i = 1; ; ) {
+			
+		}
 	}
 }

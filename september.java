b250aa1f871cb84018d8b3748a62682c52c06f4d
Ncoxy <truelonghornfan@comcast.net>
2013-09-05 08:46:06 -0600

    Added the first warmup and got it working, moved onto the second

---
+++ september.java
@@ -0,0 +1,25 @@
+public class september {
+	public static void main(String[] args) {
+		zero();
+	}
+	public static void zero() {
+		int sum = 0;
+		
+		int n = 5;
+
+		for (int i = 1; i <= n; i++) {
+			sum = sum + i;
+		}
+		System.out.println(sum);
+		
+		
+
+					
+	}
+	public static void one() {
+
+	}
+	public static void two() {
+
+	}
+}

d78be8ddd0f06e916eb3daf0da2c3746cc5da90c
Ncoxy <truelonghornfan@comcast.net>
2013-09-08 17:45:42 -0600

    First part of TERM working and used multiple print lines to show work

--- hw1.java
+++ hw1.java
@@ -3,13 +3,27 @@ import java.util.Scanner;
 
 public class hw1 {
 	public static void main(String[] args) {
-		three();
+		three(1);
 	}
-	public static void three() {
+	public static void three(double n) {
 		System.out.println("Enter a vaule for x: ");
 		Scanner s = new Scanner(System.in);
 
-		int x = s.nextInt();
+		double x = s.nextDouble();
+
+		double fact = 1;
+
+		double term = (Math.pow(x,n - 1 ));
+
+		// double sum = sum + term
+
+		
+
+		System.out.println(term);
+		System.out.println(x);
+		System.out.println(n);
+
+		
 		
 	}
 }

649887f130705ff494046214ec766b51644ecbf2
Ncoxy <truelonghornfan@comcast.net>
2013-09-08 19:40:26 -0600

    Completed five and the sequence counter, works on every number

--- hw1.java
+++ hw1.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class hw1 {
 	public static void main(String[] args) {
-		three(1);
+		// three(1);
+		five();
 	}
 	public static void three(double n) {
 		System.out.println("Enter a vaule for x: ");
@@ -16,7 +17,7 @@ public class hw1 {
 		double presum = 1;
 		double term = (Math.pow(x,n - 1 ) / (prevTerm) * (x / n));
 
-		double sum = sum + term;
+		// double sum = sum + term;
 
 		
 
@@ -24,9 +25,32 @@ public class hw1 {
 		System.out.println(x);
 		System.out.println(n);
 		System.out.println(prevTerm);
-		System.out.println(sum);
+		// System.out.println(sum);
 
 		
 		
+	}
+	public static void five() {
+		System.out.println("Enter a positive integer for N: ");
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		int seqCount = 0; 
+		 while (n != 1) {
+		 	if ((n/2)*2 == n) {
+		 		n = n/2;
+		 		System.out.println(n);
+		 		seqCount++;
+		 	}else{
+		 		n =(n * 3) + 1;
+		 		System.out.println(n);
+		 		seqCount++;
+		 	}
+		 	if(n == 1){
+		 		System.out.println("Length of the sequence is:" +seqCount);
+		 	}
+		 	
+		 }
+
 	}
 }
